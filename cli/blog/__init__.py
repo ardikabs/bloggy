6d3f699601b2b0ce23a8222a93127c6b2593371e
Ardika Bagus S <ardikabs@gmail.com>
2019-11-20 20:31:28 +0700

    Update __init__.py

--- cli/blog/__init__.py
+++ cli/blog/__init__.py
@@ -3,4 +3,4 @@
 __version__ = "0.1.0"
 __author__ = "Ardika Bagus Saputro"
 __email__ = "ardikabagus@gmail.com"
-__url__ = "https://github.com/blog-automation-terraform-ansible-stack"
+__url__ = "https://github.com/ardikabs/bloggy"
